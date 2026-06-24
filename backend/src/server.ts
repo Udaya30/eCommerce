00e622341cbfda334123ef610b89fb3712741b35
u.thiagarajan <u.thiagarajan@accenture.com>
2026-06-24 13:21:56 +0530

    serve changes

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,4 +1,3 @@
-// server.ts
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors";
@@ -9,15 +8,24 @@ import { connectDB } from "./config/db";
 import merchantRoutes from "./routes/paymentRoutes";
 
 const PORT = process.env.PORT || 5000;
-app.use("/api/merchants", merchantRoutes);
-const start = async () => {
-  await connectDB();
-  app.listen(PORT, () => console.log(`Server running on ${PORT}`));
-};
+
 app.use(cors({
   origin: ["http://localhost:5173", "https://frontend-ecommerce-8vwh.onrender.com"],
   credentials: true,
 }));
+
+app.options("*", cors({
+  origin: ["http://localhost:5173", "https://frontend-ecommerce-8vwh.onrender.com"],
+  credentials: true,
+}));
+
 app.use(cookieParser());
+
+app.use("/api/merchants", merchantRoutes);
+
+const start = async () => {
+  await connectDB();
+  app.listen(PORT, () => console.log(`Server running on ${PORT}`));
+};
 
 start();
