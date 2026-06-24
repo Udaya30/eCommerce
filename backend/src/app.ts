2a7d3d32e56f1086f8933d506cc9342e69d177e6
u.thiagarajan <u.thiagarajan@accenture.com>
2026-06-24 13:32:27 +0530

    changes

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,4 +1,6 @@
 import express from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
 import productRoutes from "./routes/productRoutes";
 import SignupRoute from "./routes/signupRoute";
 import LoginRoute from "./routes/loginRoute";
@@ -7,6 +9,12 @@ import merchantRoutes from "./routes/paymentRoutes";
 
 const app = express();
 
+app.use(cors({
+  origin: ["http://localhost:5173", "https://frontend-ecommerce-8vwh.onrender.com"],
+  credentials: true,
+}));
+
+app.use(cookieParser());
 app.use(express.json());
 
 app.use("/api/products", productRoutes);
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,6 +1,4 @@
 import dotenv from "dotenv";
-import cookieParser from "cookie-parser";
-import cors from "cors";
 dotenv.config();
 
 import app from "./app";
@@ -8,18 +6,6 @@ import { connectDB } from "./config/db";
 import merchantRoutes from "./routes/paymentRoutes";
 
 const PORT = process.env.PORT || 5000;
-
-app.use(cors({
-  origin: ["http://localhost:5173", "https://frontend-ecommerce-8vwh.onrender.com"],
-  credentials: true,
-}));
-
-app.options("*", cors({
-  origin: ["http://localhost:5173", "https://frontend-ecommerce-8vwh.onrender.com"],
-  credentials: true,
-}));
-
-app.use(cookieParser());
 
 app.use("/api/merchants", merchantRoutes);
 
